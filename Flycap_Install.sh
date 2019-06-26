ef7e108c7093fbeac11c2e4c5d6c24a64d1733c6
sungkyun123 <37562591+sungkyun123@users.noreply.github.com>
2019-06-26 11:46:18 +0900

    Update Flycap_Install.sh

--- Flycap_Install.sh
+++ Flycap_Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#--Flycapture install manual-- (ver 1.0)
+#--Flycapture install manual-- (ver 1.0.1)
 # 1. You must compile with the following command:
 #	chmod 700 install_auto.sh
 #	./install_auto.sh
@@ -8,7 +8,7 @@
 # 3. You must download the zip file with the path set to home/Downloads/.
 # 4. During installation, write y to all y / n questions. 
 # 5. write your computer name when requested.
-# 5. If you encounter problems during the installation process, please contact us at
+# 6. If you encounter problems during the installation process, please contact us at
 # sungkyun123@dgist.ac.kr
 # lekymi@dgist.ac.kr
 
